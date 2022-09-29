5f893c533fba28b897a363c185066ec3e9aa2ab8
Agustín Muñoz <agustin.imp01@gmail.com>
2022-09-29 20:29:40 -0300

    B, Tarea 3

--- Tareas/Tarea 3/B.cpp
+++ Tareas/Tarea 3/B.cpp
@@ -4,5 +4,43 @@ using namespace std;
 
 int main(){
 
-    return 0;
+    int t;
+    cin >> t;
+
+    //Son t casos, entonces loopeamos t veces
+    for (;t>0;t--){
+        int n;
+        cin>>n;
+        int a[n];
+
+        //Lleno la lista
+        for (int i = 0; i<n; i++){
+            cin >> a[i];
+        }
+
+        //Array para guardar los puntajes
+        int res[n];
+
+        //Caso base, el ultimo SIEMPRE se pasara, entonces su puntaje siempre es lo que esta en su indice
+        res[n-1] = a[n-1];
+
+        for (int i = n-2; i>=0; i--){
+            int index = i;
+            index += a[i];
+
+            //Si i + a_i se pasó, entonces su valor es lo que está en su indice
+            if (index > (n-1)) {
+                res[i] = a[i];
+            }
+            //Si no se pasó, su valor ya fue calculado y es lo que está en su indice + lo que está donde cayó
+            else{
+                res[i] = a[i] + res[index];
+            }
+        }
+
+        //Ordenamos los resultados para sacar el maximo
+        sort(res,res+n);
+        cout << res[n-1] << "\n";
+    }
+
 }
---
+++ Tareas/Tarea 3/cmake-build-debug/CMakeFiles/Tarea_3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea_3.dir/B.cpp.obj"
+  "CMakeFiles/Tarea_3.dir/B.cpp.obj.d"
+  "Tarea_3.exe"
+  "Tarea_3.exe.manifest"
+  "Tarea_3.pdb"
+  "libTarea_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
